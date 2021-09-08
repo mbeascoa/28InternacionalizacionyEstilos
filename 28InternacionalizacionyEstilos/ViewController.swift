61c1dd1d0572be9d72b4d4a4a4f3c4869477362f
mbeascoa <miguel.beascoabeastek.net>
2021-09-08 12:11:05 +0200

    8 septiembre

--- 28InternacionalizacionyEstilos/ViewController.swift
+++ 28InternacionalizacionyEstilos/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblName: UILabel!
+    
+    @IBOutlet weak var lblApellido: UILabel!
+    @IBOutlet weak var lblAge: UILabel!
+    @IBOutlet weak var etName: UITextField!
+    
+    @IBOutlet weak var etAge: UITextField!
+    @IBOutlet weak var etLastName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        lblName.text=NSLocalizedString("name", comment: "")
+        lblApellido.text=NSLocalizedString("lastname", comment: "")
+        lblAge.text=NSLocalizedString("age", comment: "")
+        lblName.textColor = UIColor(named: "MiEtiquetas")
+        lblApellido.textColor = UIColor(named: "MiEtiquetas")
+        lblAge.textColor = UIColor(named: "MiEtiquetas")
+        etName.textColor = UIColor(named: "MiCajaTexto")
+        etLastName.textColor = UIColor(named: "MiCajaTexto")
+        etAge.textColor = UIColor(named: "MiCajaTexto")
+
     }
 
 
